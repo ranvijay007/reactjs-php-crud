e8739a6d4e5defac814e98ee967dfeb239fdad7a
Ranvijay_Singh <vijaypop12@gmail.com>
2020-05-24 20:22:40 -0700

    Updates

---
+++ precache-manifest.9b89d360115019d7cfc472d9ab9d3208.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f5f1f31d48092ffef7aa7a367889e60",
+    "url": "/reactjs-php-crud/index.html"
+  },
+  {
+    "revision": "723e9617330be21d7b5e",
+    "url": "/reactjs-php-crud/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "5a1dc75342b7060e1dfb",
+    "url": "/reactjs-php-crud/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "723e9617330be21d7b5e",
+    "url": "/reactjs-php-crud/static/js/2.79139c79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-php-crud/static/js/2.79139c79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a1dc75342b7060e1dfb",
+    "url": "/reactjs-php-crud/static/js/main.aac6fe75.chunk.js"
+  },
+  {
+    "revision": "6e094a4e7cdf652e07b9",
+    "url": "/reactjs-php-crud/static/js/runtime-main.6dc2437d.js"
+  }
+]);
